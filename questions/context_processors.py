d03f18a576ec8d44dc2ec5447aa0285feb03e828
bormog <bormog@gmail.com>
2020-11-04 22:11:39 +0700

    trending block is enabled

--- questions/context_processors.py
+++ questions/context_processors.py
@@ -5,4 +5,4 @@ from .models import Question
 def trending(request):
     questions = Question.objects_related.with_num_answers().order_by('-num_answers', '-date_pub')[0:settings.QUESTIONS_PER_PAGE]
     ctx = {'trending': questions}
-    return {}
+    return ctx
