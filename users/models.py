07c3266284ebed0c5f2c08ed0439538ff51308ed
bormog <bormog@gmail.com>
2020-11-07 01:19:35 +0700

    User’s avatar thumbnail

--- users/models.py
+++ users/models.py
@@ -1,6 +1,21 @@
+from PIL import Image, ImageOps
+from io import StringIO
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 
 class UserProfile(AbstractUser):
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars/')
+
+    # todo move size to settings
+    # move to signals-callback
+    def save(self, *args, **kwargs):
+        super(UserProfile, self).save(*args, **kwargs)
+        if self.avatar:
+            file, ext = os.path.splitext(self.avatar.path)
+            image = Image.open(self.avatar.path)
+            thumbnail = ImageOps.fit(image, (200, 200), Image.ANTIALIAS)
+            thumbnail.save(file + '.thumbnail' + ext)
+
