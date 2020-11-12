2232f1b690fed0aed5b83c38ff1d18ee0f652bf9
bormog <bormog@gmail.com>
2020-11-12 13:13:21 +0700

    Ability to view own votes

--- questions/context_processors.py
+++ questions/context_processors.py
@@ -1,8 +1,23 @@
 from django.conf import settings
 from django.db.models import Count
-from .models import Question
+from .models import Question, Vote
+
 
 def trending(request):
     questions = Question.objects.filter(rank__gt=0).order_by('-rank', '-date_pub')[0:settings.QUESTIONS_PER_PAGE]
     ctx = {'trending': questions}
+    return ctx
+
+
+def user_votes(request):
+    ctx = {
+        'question': {},
+        'answer': {}
+    }
+    if request.user.is_authenticated:
+        votes = Vote.objects.filter(user=request.user).select_related('content_type').\
+            values_list('content_type__model', 'object_id', 'vote').all()
+        for (model_name, pk, vote) in votes:
+            ctx[model_name][pk] = vote
+    ctx = {'votes': ctx}
     return ctx
