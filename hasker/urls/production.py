d695fca6dcb7e4e96b6208a9b8263c18a40a586e
bormog <bormog@gmail.com>
2020-11-15 18:52:13 +0700

    serve media files via static on prod

--- hasker/urls/production.py
+++ hasker/urls/production.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('questions.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'questions.views.handler_404'
