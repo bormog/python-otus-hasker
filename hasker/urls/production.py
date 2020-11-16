149de9e2577fa6f0e4f69d877c9dfec19f97469c
bormog <bormog@gmail.com>
2020-11-17 02:19:27 +0700

    some hack for serve media

--- hasker/urls/production.py
+++ hasker/urls/production.py
@@ -1,12 +1,15 @@
+import re
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('questions.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + [
+    re_path(r'^%s(?P<path>.*)$' % re.escape(settings.MEDIA_URL.lstrip('/')), serve, kwargs=dict(document_root=settings.MEDIA_ROOT))
+]
 
 handler404 = 'questions.views.handler_404'
