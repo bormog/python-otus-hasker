c1e47e55655d04ae92e50f51d7c6f725807effb7
bormog <bormog@gmail.com>
2020-11-18 20:43:37 +0700

    add custom command

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python3 manage.py migrate --settings=hasker.settings.production
+release: python3 manage.py initadmin --settings=hasker.settings.production
 web: gunicorn hasker.wsgi --preload --log-file -
---
+++ users/management/commands/initadmin.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from users.models import UserProfile
+
+
+class Command(BaseCommand):
+    help = 'Create admin user by env variables if not exists'
+
+    def handle(self, *args, **options):
+        try:
+            email = settings.ADMIN_DEFAULT_EMAIL
+            password = settings.ADMIN_DEFAULT_PASSWORD
+        except AttributeError:
+            raise CommandError('email and password not exists in settings')
+
+        if not email or not password:
+            raise CommandError('email and password cant be empty')
+
+        username, _, _ = email.partition('@')
+
+        exists = UserProfile.objects.filter(email=email, username=username).exists()
+        if exists:
+            self.stdout.write('User already exists')
+
+        try:
+            UserProfile.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+        except Exception as e:
+            raise CommandError(str(e))
