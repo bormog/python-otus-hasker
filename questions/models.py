1af3b82d63b0718b73a5ab90c5fe487ac64e5b2a
bormog <bormog@gmail.com>
2020-12-03 11:42:23 +0700

    Move model’s manager to separate file

--- questions/models.py
+++ questions/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.db.models import Count
 from django.urls import reverse_lazy
 
+from .managers import QuestionRelationsManager
 from users.models import UserProfile
 from votes.models import RankedVoteModel
 
@@ -11,35 +11,6 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class QuestionRelationsQuerySet(models.QuerySet):
-
-    def num_answers(self):
-        return self.annotate(num_answers=Count('answers', distinct=True))
-
-    def tags(self):
-        return self.prefetch_related('tags')
-
-    def users(self):
-        return self.prefetch_related('user')
-
-
-class QuestionRelationsManager(models.Manager):
-    def get_queryset(self):
-        return QuestionRelationsQuerySet(self.model, using=self._db)
-
-    def num_answers(self):
-        return self.get_queryset().num_answers()
-
-    def tags(self):
-        return self.self.get_queryset().tags()
-
-    def users(self):
-        return self.self.get_queryset().users()
-
-    def trending(self, limit):
-        return self.get_queryset().filter(rank__gt=0).order_by('-rank', '-date_pub')[0:limit]
 
 
 class Question(RankedVoteModel):
