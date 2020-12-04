cf91830250e152665a1de18c9f3c04eb6f698a19
bormog <bormog@gmail.com>
2020-12-05 00:25:44 +0700

    Add some test for paginate

--- questions/tests/test_processors.py
+++ questions/tests/test_processors.py
@@ -1,6 +1,7 @@
 import random
 
 from django.conf import settings
+from django.shortcuts import reverse
 from django.test import TestCase
 
 from .fixtures import QuestionFactory
@@ -9,7 +10,7 @@ from .fixtures import QuestionFactory
 class TestContextProcessor(TestCase):
 
     def test_trending_question_available_in_context(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('questions:index'))
         self.assertIn('trending', response.context)
 
     def test_trending_question_is_sorted_by_rank(self):
@@ -19,13 +20,13 @@ class TestContextProcessor(TestCase):
         for rank in ranks:
             QuestionFactory.create(rank=rank)
 
-        response = self.client.get('/')
+        response = self.client.get(reverse('questions:index'))
         trending = response.context['trending']
         trending_ranks = [question.rank for question in trending]
         self.assertEqual(sorted(ranks, reverse=True), trending_ranks)
 
     def test_user_votes_available_in_context(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('questions:index'))
         self.assertIn('votes', response.context)
         self.assertIn('question', response.context['votes'])
         self.assertIn('answer', response.context['votes'])
---
+++ questions/tests/test_views.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.test import TestCase
+from django.shortcuts import reverse
+
+from .fixtures import QuestionFactory, AnswerFactory
+
+
+class TestQuestionView(TestCase):
+
+    def test_questions_paginate(self):
+        QuestionFactory.create_batch(size=settings.QUESTIONS_PER_PAGE * 2)
+        response = self.client.get(reverse('questions:index'))
+        self.assertIn('page_obj', response.context)
+        self.assertEqual(settings.QUESTIONS_PER_PAGE, len(response.context['page_obj'].object_list))
+
+    def test_question_view_answers_paginate(self):
+        question = QuestionFactory()
+        AnswerFactory.create_batch(size=settings.ANSWERS_PER_PAGE * 2, question=question)
+        response = self.client.get(reverse('questions:detail', kwargs={'pk': question.pk}))
+        self.assertIn('page_obj', response.context)
+        self.assertEqual(settings.ANSWERS_PER_PAGE, len(response.context['page_obj'].object_list))
+
+
+
